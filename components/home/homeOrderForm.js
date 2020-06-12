dcadfc6fd077979f32f143f005ea3210858767f1
Daniel Liu <daniel.g.liu14@gmail.com>
2020-06-12 16:12:14 -0400

    'message'

--- components/home/homeOrderForm.js
+++ components/home/homeOrderForm.js
@@ -3,10 +3,55 @@ import "./homeOrderForm.sass";
 import React from "react";
 
 export class HomeOrderForm extends React.Component {
+    state = {
+        numberOfOrders: 0,
+        name: "",
+        foodItem: "",
+        orderCounter: 1
+    }
+    onClickAddButton = () => {
+        this.setState({
+            orderCounter: this.state.orderCounter + 1
+        });
+        console.log("Button clicked!");
+        console.log("Name: " + this.state.name);
+        console.log("Food: " + this.state.foodItem);
+        console.log("Number of Items: " + this.state.numberOfOrders);
+        console.log("Order Counter: " + this.state.orderCounter);
+    }
+    onChangeName = (event) => {
+        const value = event.target.value;
+        this.setState({
+            name: value
+        });
+    }
+    onChangeNumber = (event) => {
+        const value = event.target.value;
+        this.setState({
+            numberOfOrders: value
+        });
+    }
+    onChangeSelect = (e) => {
+        const value = e.target.value;
+        this.setState({
+            foodItem: value
+        });
+    }
     render() {
         return (
             <section id="home-order-form">
                 <h2>New Order Form</h2>
+                <select onChange={(e) => this.onChangeSelect(e)}>
+                    <option value="burgers">Burgers</option>
+                    <option value="fries">Fries</option>
+                    <option value="shakes">Shake</option>
+                </select>
+                <span>Number: </span>
+                <input type="text" placeholder="Type here" onChange={(event) => this.onChangeNumber(event)}/>
+                <span>Your name: </span>
+                <input type="text" placeholder="Type here" onChange={(event) => this.onChangeName(event)}/>
+                <button onClick={this.onClickAddButton}>Add to Orders</button>
+
             </section>
         );
     }
--- components/home/homeOrderList.js
+++ components/home/homeOrderList.js
@@ -1,12 +1,38 @@
 "use strict";
 import "./homeOrderList.sass";
 import React from "react";
+import { HomeOrderListItem } from './homeOrderListItem.js';
+const FAKE_ORDER_LIST = [
+    {
+        id: 1,
+        food: "burgers",
+        numFood: 5,
+        name: "Joe"
+    },
+    {
+        id: 2,
+        food: "fries",
+        numFood: 8,
+        name: "Bob"
+    },
+    {
+        id: 3,
+        food: "shakes",
+        numFood: 2,
+        name: "Jason"
+    }
+];
 
 export class HomeOrderList extends React.Component {
     render() {
+        const orders = FAKE_ORDER_LIST;
+        const items = orders.map((order, index) => (<li key={index}><h1>{order.name}</h1></li>));
         return (
             <section id="home-order-list">
                 <h1>Order List</h1>
+                <ul>
+                    {items}
+                </ul>
             </section>
         );
     }
